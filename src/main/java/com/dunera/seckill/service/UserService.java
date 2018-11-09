587e0cd56c1252caf161b0c2590aeb11efacd638
lyx <yunxin@scapital.com>
2018-11-10 00:11:54 +0800

    update user service

---
+++ src/main/java/com/dunera/seckill/service/UserService.java
@@ -0,0 +1,19 @@
+package com.dunera.seckill.service;
+
+import com.dunera.seckill.pojo.User;
+
+/**
+ * @author lyx
+ * @date 2018/11/9
+ */
+public interface UserService {
+
+    void addUser(String userName, String password, int avatar);
+
+    boolean checkPassword(String userName, String password);
+
+    User getUserByUserName(String userName);
+
+    User getUserByUserId(Integer userId);
+
+}
---
+++ src/main/java/com/dunera/seckill/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.dunera.seckill.service.impl;
+
+import com.dunera.seckill.dao.UserMapper;
+import com.dunera.seckill.pojo.User;
+import com.dunera.seckill.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lyx
+ * @date 2018/11/10
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public void addUser(String userName, String password, int avatar) {
+        User user = new User();
+        user.setUserName(userName);
+        user.setMd5password(password);
+        user.setAvater(avatar);
+        userMapper.insert(user);
+    }
+
+    @Override
+    public boolean checkPassword(String userName, String password) {
+        return false;
+    }
+
+    @Override
+    public User getUserByUserName(String userName) {
+        return null;
+    }
+
+    @Override
+    public User getUserByUserId(Integer userId) {
+        return null;
+    }
+}
