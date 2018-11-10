90b68e30624b81048eac94f46f009dc5d10cc6d1
lyx <yunxin@scapital.com>
2018-11-10 23:41:29 +0800

    update

---
+++ src/main/java/com/dunera/seckill/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.dunera.seckill.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lyx
+ * @date 2018/11/10
+ */
+@Configuration
+public class RedisConfig {
+
+}
