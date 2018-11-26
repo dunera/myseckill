1ff9210e18deb9fce8d62c7d2fac3605782f1412
lyx <458098102@qq.com>
2018-11-26 18:32:51 +0800

    update test resource

--- src/test/java/com/dunera/seckill/common/redis/RedisHandlerTest.java
+++ src/test/java/com/dunera/seckill/common/redis/RedisHandlerTest.java
@@ -1,5 +1,6 @@
-package com.dunera.seckill;
+package com.dunera.seckill.common.redis;
 
+import com.dunera.seckill.SeckillApplication;
 import com.dunera.seckill.config.RedisProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,16 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * @author lyx
+ * @date 2018/11/22
+ */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class SeckillApplicationTests {
+@SpringBootTest(classes = SeckillApplication.class)
+public class RedisHandlerTest {
 
     @Autowired
     private RedisProperties properties;
 
     @Test
-    public void contextLoads() {
-
+    public void set() {
+        System.out.println(properties.getHost());
     }
-
 }
